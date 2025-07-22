4d7270f5924b1a6caa02683e9487f21f0d3c98fa
Krishna Ningaraddi <krishna.ningaraddi08@gmail.com>
2025-07-22 23:35:04 +0530

    Basics

--- Basics/Test.js
+++ Basics/Test.js
@@ -1,1 +1,22 @@
 console.log("Hello Hackers!")
+
+// Variables and Contants
+
+const accountId=144553
+let accountEmail="dumma@dablya.com"
+var accountPassword="123456" 
+/* 
+now adays we don't use it as we have scope issues 
+prefer not to use var because of issue in block scope and functional scope
+
+*/
+
+accountCity="Mysore" // this is not a right way to declare a variable
+
+let accountStatus; // this containes the undefined value
+
+// accountId=125431 // Can't be modified as it is a constant variable
+console.log(accountId)
+
+// Instead of printing all the variables one by one using console log we can use table method
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStatus])

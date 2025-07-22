bc859018678aa97c60aca40f5f5990945351f489
Krishna Ningaraddi <krishna.ningaraddi08@gmail.com>
2025-07-23 00:07:51 +0530

    DataTypes discussion

---
+++ Basics/DataTypes.js
@@ -0,0 +1,23 @@
+"Use strict"; // treats all Js code as newer version
+
+// alert("Hello") // we are using node.js not browser so it will throw error
+
+let name="Virat Kohli"
+let jurseyNo=18
+let isLoggedIn=false
+
+//number -> 2^53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// symbol 
+// object
+
+console.log(typeof "krishna");
+console.log(typeof 56);
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
+
+

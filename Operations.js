db6075faef45391b0a6b5b797c7e83747a2b6acf
Krishna Ningaraddi <krishna.ningaraddi08@gmail.com>
2025-07-30 17:32:31 +0530

    Operations

---
+++ Operations.js
@@ -0,0 +1,44 @@
+// ---------------Operations------------------
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+let str1="Hello"
+let str2=" KrishReddy"
+
+// console.log(str1+str2);
+
+// console.log((1+"2"));
+// console.log((1+"2"+3));
+// console.log(("1"+2+3));
+// console.log((1+2+"3"));
+// console.log(("1"+2));
+
+
+// console.log(+true)
+// console.log(true+false) // 1 + 0 = 1
+// console.log(+"");
+
+// let num1,num2,num3
+
+// num1=num2=num3=2 + 3
+
+// console.table([a=num1,num2,num3])
+
+let gameCounter = 100
+let temp=++gameCounter // value increaments to 101 and then gets assigned
+console.log("gamecounter "+ gameCounter);
+console.log("temp "+ temp);
+
+let gameCounter1 = 100
+let temp1=gameCounter1++ // value will be 100
+console.log("gamecounter1 "+ gameCounter1);
+console.log("temp1 "+ temp1);
+
+
+
+
+
+
+
